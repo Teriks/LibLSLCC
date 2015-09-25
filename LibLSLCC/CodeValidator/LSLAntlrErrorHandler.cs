662fe1ff4e221f32ce9043c98c7b11faf6df051c
Teriks <Teriks@users.noreply.github.com>
2015-09-25 05:47:22 -0500

    more permissive BSD license

--- LibLSLCC/CodeValidator/LSLAntlrErrorHandler.cs
+++ LibLSLCC/CodeValidator/LSLAntlrErrorHandler.cs
@@ -1,30 +1,46 @@
 ﻿#region FileInfo
-
+// 
 // 
 // File: LSLAntlrErrorHandler.cs
 // 
-// Author/Copyright:  Teriks
-// 
-// Last Compile: 24/09/2015 @ 9:25 PM
+// Last Compile: 25/09/2015 @ 5:46 AM
 // 
 // Creation Date: 21/08/2015 @ 12:22 AM
 // 
+// ============================================================
+// ============================================================
+// 
 // 
 // This file is part of LibLSLCC.
-// LibLSLCC is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// LibLSLCC is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// You should have received a copy of the GNU General Public License
-// along with LibLSLCC.  If not, see <http://www.gnu.org/licenses/>.
 // 
-
+// LibLSLCC is distributed under the following BSD 3-Clause License
+// 
+// Copyright (c) 2015, Teriks
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+// 
+// 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+// 
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer
+//     in the documentation and/or other materials provided with the distribution.
+// 
+// 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived
+//     from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+// ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// 
+// ============================================================
+// ============================================================
+// 
+// 
 #endregion
-
 #region Imports
 
 using Antlr4.Runtime;
