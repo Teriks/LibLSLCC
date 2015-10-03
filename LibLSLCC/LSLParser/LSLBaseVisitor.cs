fa23d5086b4e86f7e796ec08fda29e4801ee410a
Teriks <Teriks@users.noreply.github.com>
2015-10-02 21:11:55 -0500

    Fixed path seperators in 'BeforeBuild' target of the LibLSLCC project file, this target runs an MSBuild Exec task to build the parser from LSL.g4 using ANTLR4

--- LibLSLCC/LSLParser/LSLBaseVisitor.cs
+++ LibLSLCC/LSLParser/LSLBaseVisitor.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-// Generated from J:\Projects\csharp\LibLSLCC\LibLSLCC\/LSLParser/LSL.g4 by ANTLR 4.5.1
+// Generated from J:\Projects\csharp\LibLSLCC\LibLSLCC\LSLParser\LSL.g4 by ANTLR 4.5.1
 
 // Unreachable code detected
 #pragma warning disable 0162
