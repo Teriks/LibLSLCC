b5b4ddde00842c2d04500827ccf6b1aa71f61ce0
Teriks <Teriks@users.noreply.github.com>
2015-10-31 19:04:24 -0500

    mend

---
+++ LibLSLCC/Formatter/LSLCodeFormatter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using LibLSLCC.CodeValidator.Nodes.Interfaces;
+using LibLSLCC.Formatter.Visitor;
+
+namespace LibLSLCC.Formatter
+{
+    public class LSLCodeFormatter
+    {
+        public void Format(string sourceReference, ILSLCompilationUnitNode node, TextWriter writer,
+            bool closeStream = true)
+        {
+            var formatter = new LSLCodeFormatterVisitor();
+            formatter.WriteAndFlush(sourceReference, node, writer, closeStream);
+        }
+    }
+}
