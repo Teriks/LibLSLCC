5d73870ef5197673b985380c6e25598998dd03c0
Teriks <Teriks@users.noreply.github.com>
2015-12-01 02:42:10 -0600

    Fix CompletionUI assembly attributes

--- LSLCCEditor.CompletionUI/Properties/AssemblyInfo.cs
+++ LSLCCEditor.CompletionUI/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LibLSLCC.CompletionUI")]
+[assembly: AssemblyTitle("LSLCCEditor.CompletionUI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LibLSLCC.CompletionUI")]
+[assembly: AssemblyProduct("LSLCCEditor.CompletionUI")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
