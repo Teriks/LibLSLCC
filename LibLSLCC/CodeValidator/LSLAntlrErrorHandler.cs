edf87529f749c7dd8a612d2b3fb95d4a5528f4a1
Teriks <Teriks@users.noreply.github.com>
2016-02-07 01:57:18 -0600

    Fix up assignment expression syntax errors

--- LibLSLCC/CodeValidator/LSLAntlrErrorHandler.cs
+++ LibLSLCC/CodeValidator/LSLAntlrErrorHandler.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using Antlr4.Runtime;
 using LibLSLCC.CodeValidator.Components.Interfaces;
 using LibLSLCC.CodeValidator.Primitives;
@@ -8,7 +10,7 @@ namespace LibLSLCC.CodeValidator
     internal class LSLAntlrErrorHandler : IAntlrErrorListener<IToken>
     {
         private static readonly Regex NonLValueAssignmentError =
-            new Regex("mismatched input '[*+-/%]?=' expecting {';', '==', '!=', '<', '>', '<=', '>=', '>>', '<<', '-', '\\+', '\\+\\+', '--', '\\*', '/', '%', '\\|', '&', '\\^', '&&', '\\|\\|'}");
+            new Regex("mismatched input '[*+-/%]?=' expecting {(.*?)}");
 
         private readonly ILSLSyntaxErrorListener _errorListener;
 
@@ -24,10 +26,19 @@ namespace LibLSLCC.CodeValidator
         {
             HasErrors = true;
 
+            var m = NonLValueAssignmentError.Match(msg);
 
-            if (NonLValueAssignmentError.IsMatch(msg))
+            var expected = new HashSet<string>(m.Groups[1].ToString().Split(',').Select(x=>x.Trim('\'', ' ')));
+
+            if (m.Success && expected.Contains("*") && !(
+                   expected.Contains("TYPE")
+                || expected.Contains("ID")
+                || expected.Contains("INT")
+                || expected.Contains("FLOAT")
+                || expected.Contains("HEX_LITERAL")
+                || expected.Contains("QUOTED_STRING")
+                ))
             {
-
                 _errorListener.AssignmentToUnassignableExpression(new LSLSourceCodeRange(offendingSymbol), offendingSymbol.Text);
             }
             else
