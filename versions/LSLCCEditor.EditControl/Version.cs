54fb67d5643fe70929ff6a7c6b12b61915039c40
Teriks <Teriks@users.noreply.github.com>
2016-02-16 10:57:16 -0600

    Improve MainWindow UI look

--- versions/LSLCCEditor.EditControl/Version.cs
+++ versions/LSLCCEditor.EditControl/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0.10")]
-[assembly: AssemblyFileVersion("1.0.0.10")]
+[assembly: AssemblyVersion("1.0.0.11")]
+[assembly: AssemblyFileVersion("1.0.0.11")]
