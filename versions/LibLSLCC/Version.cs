0c1451b5f76f15f2f9808cd654ee7e94b423d9ae
Teriks <Teriks@users.noreply.github.com>
2016-02-22 23:29:47 -0600

    Hide internal members from Doxygen

--- versions/LibLSLCC/Version.cs
+++ versions/LibLSLCC/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0.95")]
-[assembly: AssemblyFileVersion("1.0.0.95")]
+[assembly: AssemblyVersion("1.0.0.96")]
+[assembly: AssemblyFileVersion("1.0.0.96")]
