34c0dd88270045892598fed700d43c1179ba4a30
Teriks <Teriks@users.noreply.github.com>
2016-02-23 22:31:36 -0600

    Version adjust

--- versions/LSLCCEditor/Version.cs
+++ versions/LSLCCEditor/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0.47")]
-[assembly: AssemblyFileVersion("1.0.0.47")]
+[assembly: AssemblyVersion("1.0.0.49")]
+[assembly: AssemblyFileVersion("1.0.0.49")]
