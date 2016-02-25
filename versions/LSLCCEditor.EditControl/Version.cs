5130f4f87654b039d6c004f6f457ce31ef398beb
Teriks <Teriks@users.noreply.github.com>
2016-02-24 20:31:52 -0600

    Format

--- versions/LSLCCEditor.EditControl/Version.cs
+++ versions/LSLCCEditor.EditControl/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0.17")]
-[assembly: AssemblyFileVersion("1.0.0.17")]
+[assembly: AssemblyVersion("1.0.0.18")]
+[assembly: AssemblyFileVersion("1.0.0.18")]
--- versions/LibLSLCC/Version.cs
+++ versions/LibLSLCC/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0.108")]
-[assembly: AssemblyFileVersion("1.0.0.108")]
+[assembly: AssemblyVersion("1.0.0.109")]
+[assembly: AssemblyFileVersion("1.0.0.109")]
