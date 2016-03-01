902b7603f875b07b5dc6f415715fc0f0a8191387
Teriks <Teriks@users.noreply.github.com>
2016-03-01 00:13:56 -0600

    Fix a few sandcastle settings

--- versions/LibLSLCC/Version.cs
+++ versions/LibLSLCC/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0.158")]
-[assembly: AssemblyFileVersion("1.0.0.158")]
+[assembly: AssemblyVersion("1.0.0.159")]
+[assembly: AssemblyFileVersion("1.0.0.159")]
