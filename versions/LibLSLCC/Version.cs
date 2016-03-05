36564ae12806716cc7a72099f9587f4ade2812b0
Teriks <Teriks@users.noreply.github.com>
2016-03-05 04:36:07 -0600

    Clean up default library data

--- versions/LibLSLCC/Version.cs
+++ versions/LibLSLCC/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0.180")]
-[assembly: AssemblyFileVersion("1.0.0.180")]
+[assembly: AssemblyVersion("1.0.0.181")]
+[assembly: AssemblyFileVersion("1.0.0.181")]
