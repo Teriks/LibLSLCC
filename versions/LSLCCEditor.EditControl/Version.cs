20b3af3de3f104ef8afdb2da564d11badf037bfa
Teriks <Teriks@users.noreply.github.com>
2016-03-09 08:01:30 -0600

    Cleanup library data doc a bit

--- versions/LSLCCEditor.EditControl/Version.cs
+++ versions/LSLCCEditor.EditControl/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0.29")]
-[assembly: AssemblyFileVersion("1.0.0.29")]
+[assembly: AssemblyVersion("1.0.0.30")]
+[assembly: AssemblyFileVersion("1.0.0.30")]
--- versions/LibLSLCC/Version.cs
+++ versions/LibLSLCC/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0.194")]
-[assembly: AssemblyFileVersion("1.0.0.194")]
+[assembly: AssemblyVersion("1.0.0.195")]
+[assembly: AssemblyFileVersion("1.0.0.195")]
