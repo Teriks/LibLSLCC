f023234c3f9fc80099dae6c751b0deb76d7f5a86
Teriks <Teriks@users.noreply.github.com>
2016-03-30 01:18:30 -0500

    Fix version.py on linux
    subprocess.check_output behaves a bit differently on linux, and or behaves incorrectly on windows and I coded against it.
    
    Patch increment.  The snapshot tag used for calculating the revision number
    no longer exists.  Revision is now commits to assembly since 1.0.0.199.

--- versions/LibraryDataScrapingTool/Version.cs
+++ versions/LibraryDataScrapingTool/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.0.24")]
-[assembly: AssemblyFileVersion("0.1.0.24")]
+[assembly: AssemblyVersion("0.1.1.1")]
+[assembly: AssemblyFileVersion("0.1.1.1")]
