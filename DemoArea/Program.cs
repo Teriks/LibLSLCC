95b448630dfc5a06642aa499ce03979b8ffee5fe
Teriks <Teriks@users.noreply.github.com>
2016-04-01 08:01:45 -0500

    remove kruft from DemoArea

--- DemoArea/Program.cs
+++ DemoArea/Program.cs
@@ -399,7 +399,6 @@ namespace DemoArea
                 Deprecated = true
             });
 
-            string tt =  LSLFormatTools.NormalizeFloatString("0e-0f");
             Console.WriteLine("Methods....\n\n");
 
 
--- versions/LibraryDataScrapingTool/Version.cs
+++ versions/LibraryDataScrapingTool/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.2.5")]
-[assembly: AssemblyFileVersion("0.1.2.5")]
+[assembly: AssemblyVersion("0.1.2.6")]
+[assembly: AssemblyFileVersion("0.1.2.6")]
