e344b6ec85bc7975c526f6ca50f60ba27ee8f6e7
Teriks <Teriks@users.noreply.github.com>
2016-04-01 17:33:47 -0500

    Fix library data encoding
    Visual Studios keeps changing it to UCS-2 causing git to think its a
    binary...

--- versions/LibraryDataScrapingTool/Version.cs
+++ versions/LibraryDataScrapingTool/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.2.10")]
-[assembly: AssemblyFileVersion("0.1.2.10")]
+[assembly: AssemblyVersion("0.1.2.11")]
+[assembly: AssemblyFileVersion("0.1.2.11")]
