2d626db4a0801acfe8cf7c821354596d7dbf8043
Teriks <Teriks@users.noreply.github.com>
2016-04-01 17:37:55 -0500

    Update xml encoding tag in library data

--- versions/LSLCCEditor/Version.cs
+++ versions/LSLCCEditor/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.1.1.11")]
-[assembly: AssemblyFileVersion("1.1.1.11")]
+[assembly: AssemblyVersion("1.1.1.12")]
+[assembly: AssemblyFileVersion("1.1.1.12")]
--- versions/LibraryDataScrapingTool/Version.cs
+++ versions/LibraryDataScrapingTool/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.2.11")]
-[assembly: AssemblyFileVersion("0.1.2.11")]
+[assembly: AssemblyVersion("0.1.2.12")]
+[assembly: AssemblyFileVersion("0.1.2.12")]
