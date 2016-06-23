6f61694d7cbfa26f8f9b8602c20cd3c7f6c9ad23
Teriks <Teriks@users.noreply.github.com>
2016-06-22 22:57:32 -0500

    Increment versions

--- versions/LSLCCEditor.CompletionUI/Version.cs
+++ versions/LSLCCEditor.CompletionUI/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.1.46")]
-[assembly: AssemblyFileVersion("1.0.1.46")]
+[assembly: AssemblyVersion("1.0.1.48")]
+[assembly: AssemblyFileVersion("1.0.1.48")]
