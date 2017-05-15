341a667dec576c95247727051b5dd19ca67732ee
Teriks <Teriks@users.noreply.github.com>
2017-05-14 19:45:45 -0500

    Update versions

--- versions/LSLCCEditor/Version.cs
+++ versions/LSLCCEditor/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.2.1.45")]
-[assembly: AssemblyFileVersion("1.2.1.45")]
+[assembly: AssemblyVersion("1.2.1.47")]
+[assembly: AssemblyFileVersion("1.2.1.47")]
--- versions/LibraryDataScrapingTool/Version.cs
+++ versions/LibraryDataScrapingTool/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.2.98")]
-[assembly: AssemblyFileVersion("0.1.2.98")]
+[assembly: AssemblyVersion("0.1.2.100")]
+[assembly: AssemblyFileVersion("0.1.2.100")]
