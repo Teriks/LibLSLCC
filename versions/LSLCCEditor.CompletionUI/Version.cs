dc8acde011c29f56f58a73931c879552e37b22b6
Teriks <Teriks@users.noreply.github.com>
2017-08-16 17:21:10 -0500

    Update Versions

--- versions/LSLCCEditor.CompletionUI/Version.cs
+++ versions/LSLCCEditor.CompletionUI/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.1.126")]
-[assembly: AssemblyFileVersion("1.0.1.126")]
+[assembly: AssemblyVersion("1.0.1.129")]
+[assembly: AssemblyFileVersion("1.0.1.129")]
--- versions/LSLCCEditor.EditControl/Version.cs
+++ versions/LSLCCEditor.EditControl/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.2.120")]
-[assembly: AssemblyFileVersion("1.0.2.120")]
+[assembly: AssemblyVersion("1.0.2.123")]
+[assembly: AssemblyFileVersion("1.0.2.123")]
