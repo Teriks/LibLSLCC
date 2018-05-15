65a147a63d6a4f8e0885ac29f49c64e17cf152ac
Teriks <Teriks@users.noreply.github.com>
2018-05-15 09:38:39 -0500

    Update versions file

--- versions/LSLCCEditor.Utility/Version.cs
+++ versions/LSLCCEditor.Utility/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.1.136")]
-[assembly: AssemblyFileVersion("0.1.1.136")]
+[assembly: AssemblyVersion("0.1.1.138")]
+[assembly: AssemblyFileVersion("0.1.1.138")]
