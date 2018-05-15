6793b38d548b4c5afbed824a9e306779b631ff74
Teriks <Teriks@users.noreply.github.com>
2018-05-15 09:41:41 -0500

    Update versions

--- versions/LibraryDataScrapingTool/Version.cs
+++ versions/LibraryDataScrapingTool/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.2.127")]
-[assembly: AssemblyFileVersion("0.1.2.127")]
+[assembly: AssemblyVersion("0.1.2.129")]
+[assembly: AssemblyFileVersion("0.1.2.129")]
